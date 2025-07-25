da8268d9ee9cd263949e573e2235714e46e7b4ce
Juan Gómez <gb.jc@outlook.com>
2025-07-25 13:58:34 -0400

    ReactQuery and useEffect

--- app/bookmarks/page.tsx
+++ app/bookmarks/page.tsx
@@ -1,20 +1,25 @@
+"use client"
+
 import { Heading, Text } from "@chakra-ui/react"
 
 import { Bookmark } from "@/components/bookmark"
-// import { BookmarkType } from './schema';
-import { orm } from "./db";
+import { useQuery } from "@tanstack/react-query";
+import { BookmarkType } from "./schema";
 
-export default async function Bookmarks() {
-
-  // const bookmarksResponse = await fetch("http://localhost:3000/bookmarks/api", {next: { tags: ["bookmarks"] }})
-  // const bookmarks = await bookmarksResponse.json() as { data: BookmarkType[] }
-
-  const bookmarks = await orm.query.bookmarks.findMany({
-    limit: 10,
-    with: {
-      author: true,
-    },
+export default function Bookmarks() {
+  const { data, status } =
+  useQuery<BookmarkType[]>({
+    queryKey: ['bookmarks'],
+    queryFn: async() => {
+      return fetch("http://localhost:3000/bookmarks/api")
+        .then(response => response.json())
+        .then(( { data } ) => {
+          return data
+        })
+    }
   })
+
+  console.log({status})
 
   return (
     <main className="mt-12">
@@ -28,7 +33,7 @@ export default async function Bookmarks() {
       </header>
 
       <ul className="text-lg mt-10">
-        {bookmarks?.data.map((bookmark) => (
+        {data?.map((bookmark) => (
           <li className="border-b-2 py-4 px-6 my-2" key={bookmark.id}>
             <Bookmark {...bookmark} />
             {/* <div className="my-1 text-gray-600 text-xs ml-7">
